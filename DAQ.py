5732c9930e6955128e26902fa0ba3faf07461913
Tom <tom.p.a.warner@gmail.com>
2019-07-11 14:29:14 +0100

    Add DoTriggeredCoTask
    Adds triggered task if there are no analogue channels

--- DAQ.py
+++ DAQ.py
@@ -239,6 +239,55 @@ class AnalogOutput(Task):
 
 
 # region [MultiTasks]
+class DoTriggeredCoTask:
+    def __init__(self, do_device, co_device, samp_rate, secs, write, trigger_source):
+        self.do_handle = TaskHandle(0)
+        self.co_handle = TaskHandle(1)
+
+
+        DAQmxCreateTask('', byref(self.do_handle))
+        DAQmxCreateTask('', byref(self.co_handle))
+
+        DAQmxCreateCOPulseChanFreq(self.co_handle, '/cDAQ1/Ctr0', '', DAQmx_Val_Hz, DAQmx_Val_Low, 0.0, samp_rate, 0.5)
+        DAQmxCreateDOChan(self.do_handle, do_device, '', DAQmx_Val_ChanForAllLines)
+        
+
+        DAQmxCfgDigEdgeStartTrig(self.co_handle, trigger_source, DAQmx_Val_Rising)
+        self.totalLength = numpy.uint64(samp_rate * secs)
+        self.secs = secs
+        self.sampsPerChanWritten = int32()
+        self.write = Util.binary_to_digital_map(write)
+        self.sampsPerChan = self.write.shape[1]
+        self.write = numpy.sum(self.write, axis=0)
+
+
+        DAQmxCfgImplicitTiming(self.co_handle, DAQmx_Val_FiniteSamps, self.totalLength)
+        DAQmxCfgSampClkTiming(self.do_handle, '/cDAQ1/Ctr0InternalOutput', samp_rate, DAQmx_Val_Rising, DAQmx_Val_FiniteSamps,
+                              numpy.uint64(self.totalLength))
+        
+    def DoTask(self):
+
+        DAQmxWriteDigitalU32(self.do_handle, self.sampsPerChan, 0, -1, DAQmx_Val_GroupByChannel, self.write,
+                             byref(self.sampsPerChanWritten), None)
+
+
+        DAQmxStartTask(self.do_handle)
+        DAQmxStartTask(self.co_handle)
+  
+        DAQmxWaitUntilTaskDone(self.co_handle, 100)
+        DAQmxWaitUntilTaskDone(self.do_handle, 100)
+
+        self.ClearTasks()
+
+    def ClearTasks(self):
+        time.sleep(0.05)
+
+        DAQmxStopTask(self.do_handle)
+        DAQmxStopTask(self.co_handle)
+ 
+        DAQmxClearTask(self.do_handle)
+        DAQmxClearTask(self.co_handle)
+
 
 class DoCoTask:
     def __init__(self, do_device, co_device, samp_rate, secs, write):
@@ -269,7 +318,9 @@ class DoCoTask:
 
         DAQmxStartTask(self.do_handle)
         DAQmxStartTask(self.co_handle)
-        time.sleep(self.secs + 0.05)
+        DAQmxWaitUntilTaskDone(self.co_handle, 100)
+        DAQmxWaitUntilTaskDone(self.do_handle, 100)
+
 
         self.ClearTasks()
 
@@ -360,6 +411,7 @@ class DoAiTriggeredMultiTask:
                               numpy.uint64(self.totalLength))
 
     def DoTask(self):
+
         DAQmxWriteDigitalU32(self.do_handle, self.sampsPerChan, 0, -1, DAQmx_Val_GroupByChannel, self.write,
                              byref(self.sampsPerChanWritten), None)
 
